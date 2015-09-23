ecd7ee3e75880ea0bbe4b46773e0369f7ac10c06
Sebastien Soudan <sebastien.soudan@gmail.com>
2015-09-23 09:59:09 +0200

    [enhancement] moved dashboard test to ginkgo

--- dashboard/dashboard_test.go
+++ dashboard/dashboard_test.go
@@ -16,18 +16,126 @@ under the License.
 
 /*
 * @Author: Sebastien Soudan
-* @Date:   2015-09-20 23:11:55
+* @Date:   2015-09-21 15:42:21
 * @Last Modified by:   Sebastien Soudan
-* @Last Modified time: 2015-09-20 23:44:25
+* @Last Modified time: 2015-09-23 09:58:41
  */
 
 package dashboard
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
-	"time"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
+
+var _ = Describe("dashboard", func() {
+
+	var (
+		dashboard  *Dashboard
+		aaaHandler testHandler
+		bbbHandler testHandler
+		c          chan interface{}
+	)
+
+	BeforeEach(func() {
+		dashboard = New()
+
+		aaaHandler = testHandler{}
+		dashboard.RegisterMessageHandler("AAA", &aaaHandler)
+
+		bbbHandler = testHandler{}
+		dashboard.RegisterMessageHandler("BBB", &bbbHandler)
+
+		c = make(chan interface{})
+
+		dashboard.SetInputChan(c)
+
+		dashboard.Start()
+	})
+
+	Describe("without channels", func() {
+
+		It("state is false by default", func() {
+			Expect(aaaHandler.state).To(BeFalse())
+			Expect(bbbHandler.state).To(BeFalse())
+		})
+
+		It("changes with messages", func() {
+			m := NewMessage(map[string]bool{"AAA": true})
+
+			dashboard.processMessage(m.(message))
+
+			Expect(aaaHandler.state).To(BeTrue())
+
+			Expect(bbbHandler.state).To(BeFalse())
+
+			m = NewMessage(map[string]bool{"BBB": true})
+
+			dashboard.processMessage(m.(message))
+
+			Expect(aaaHandler.state).To(BeTrue())
+			Expect(bbbHandler.state).To(BeTrue())
+
+			m = NewMessage(map[string]bool{"BBB": false})
+
+			dashboard.processMessage(m.(message))
+
+			Expect(aaaHandler.state).To(BeTrue())
+			Expect(bbbHandler.state).To(BeFalse())
+
+			m = NewMessage(map[string]bool{"BBB": false, "AAA": false})
+
+			dashboard.processMessage(m.(message))
+
+			Expect(aaaHandler.state).To(BeFalse())
+			Expect(bbbHandler.state).To(BeFalse())
+		})
+
+	})
+
+	Describe("with channels", func() {
+
+		It("state is false by default", func() {
+			Expect(aaaHandler.state).To(BeFalse())
+			Expect(bbbHandler.state).To(BeFalse())
+		})
+
+		It("changes with messages", func() {
+
+			m := NewMessage(map[string]bool{"AAA": true})
+
+			c <- m
+
+			Eventually(aaaHandler.state).Should(BeTrue())
+			Consistently(bbbHandler.state).Should(BeFalse())
+		})
+
+	})
+
+	Describe("dashboard shutdown", func() {
+
+		It("resets leds when shut down", func() {
+			Expect(aaaHandler.state).To(BeFalse())
+			Expect(bbbHandler.state).To(BeFalse())
+
+			m := NewMessage(map[string]bool{
+				"BBB": true,
+				"AAA": true,
+			})
+
+			c <- m
+
+			Eventually(aaaHandler.state).Should(BeTrue())
+			Eventually(bbbHandler.state).Should(BeTrue())
+
+			dashboard.Shutdown()
+
+			Eventually(aaaHandler.state).Should(BeFalse())
+			Eventually(bbbHandler.state).Should(BeFalse())
+		})
+
+	})
+})
 
 type testHandler struct {
 	state bool
@@ -40,132 +148,4 @@ func (h *testHandler) Enable() error {
 func (h *testHandler) Disable() error {
 	h.state = false
 	return nil
-}
-
-func TestDashboardChangesLEDStates(t *testing.T) {
-
-	dashboard := New()
-
-	aaaHandler := testHandler{}
-	dashboard.RegisterMessageHandler("AAA", &aaaHandler)
-
-	bbbHandler := testHandler{}
-	dashboard.RegisterMessageHandler("BBB", &bbbHandler)
-
-	c := make(chan interface{})
-
-	dashboard.SetInputChan(c)
-
-	dashboard.Start()
-
-	assert.EqualValues(t, false, aaaHandler.state, "state is false by default")
-	assert.EqualValues(t, false, bbbHandler.state, "state is false by default (2)")
-
-	m := NewMessage(map[string]bool{"AAA": true})
-
-	dashboard.processMessage(m.(message))
-
-	assert.EqualValues(t, true, aaaHandler.state, "previous message should have set it to true")
-	assert.EqualValues(t, false, bbbHandler.state, "state should not have changed for BBB")
-
-	m = NewMessage(map[string]bool{"BBB": true})
-
-	dashboard.processMessage(m.(message))
-
-	assert.EqualValues(t, true, aaaHandler.state, "state should not have changed for AAA")
-	assert.EqualValues(t, true, bbbHandler.state, "BBB should have changed to true")
-
-	m = NewMessage(map[string]bool{"BBB": false})
-
-	dashboard.processMessage(m.(message))
-
-	assert.EqualValues(t, true, aaaHandler.state, "still true")
-	assert.EqualValues(t, false, bbbHandler.state, "and back to false")
-
-	m = NewMessage(map[string]bool{"BBB": false, "AAA": false})
-
-	dashboard.processMessage(m.(message))
-
-	assert.EqualValues(t, false, aaaHandler.state, "false now")
-	assert.EqualValues(t, false, bbbHandler.state, "false too")
-
-}
-
-func TestDashboardWithChannel(t *testing.T) {
-	dashboard := New()
-
-	aaaHandler := testHandler{}
-	dashboard.RegisterMessageHandler("AAA", &aaaHandler)
-
-	bbbHandler := testHandler{}
-	dashboard.RegisterMessageHandler("BBB", &bbbHandler)
-
-	c := make(chan interface{})
-
-	dashboard.SetInputChan(c)
-
-	dashboard.Start()
-
-	assert.EqualValues(t, false, aaaHandler.state, "state is false by default")
-	assert.EqualValues(t, false, bbbHandler.state, "state is false by default (2)")
-
-	m := NewMessage(map[string]bool{"AAA": true})
-
-	c <- m
-
-	// Have to wait for the update to be propagated
-	for !aaaHandler.state {
-		time.Sleep(1 * time.Second)
-	}
-
-	assert.EqualValues(t, true, aaaHandler.state, "previous message should have set it to true")
-	assert.EqualValues(t, false, bbbHandler.state, "state should not have changed for BBB")
-
-}
-
-func TestDashboardShutdown(t *testing.T) {
-	dashboard := New()
-
-	aaaHandler := testHandler{}
-	dashboard.RegisterMessageHandler("AAA", &aaaHandler)
-
-	bbbHandler := testHandler{}
-	dashboard.RegisterMessageHandler("BBB", &bbbHandler)
-
-	c := make(chan interface{})
-
-	dashboard.SetInputChan(c)
-
-	dashboard.Start()
-
-	assert.EqualValues(t, false, aaaHandler.state, "state is false by default")
-	assert.EqualValues(t, false, bbbHandler.state, "state is false by default (2)")
-
-	m := NewMessage(map[string]bool{
-		"BBB": true,
-		"AAA": true,
-	})
-
-	c <- m
-
-	// Have to wait for the update to be propagated
-	for !(aaaHandler.state && bbbHandler.state) {
-		log.Debug("aaaHandler.state = %v", aaaHandler.state)
-		log.Debug("bbbHandler.state = %v", bbbHandler.state)
-		time.Sleep(1 * time.Second)
-	}
-
-	assert.EqualValues(t, true, aaaHandler.state, "previous message should have set it to true")
-	assert.EqualValues(t, true, bbbHandler.state, "state should have changed for BBB too")
-
-	dashboard.Shutdown()
-
-	for !(!aaaHandler.state && !bbbHandler.state) {
-		log.Debug("aaaHandler.state = %v", aaaHandler.state)
-		log.Debug("bbbHandler.state = %v", bbbHandler.state)
-		time.Sleep(1 * time.Second)
-	}
-
-	assert.EqualValues(t, false, aaaHandler.state, "shutdown effect")
-	assert.EqualValues(t, false, bbbHandler.state, "second shutdown effect")
 }
